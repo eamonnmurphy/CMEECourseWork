19c2342e80ea996d343a5902f0efe10c063a3502
Eamonn Murphy <eamonn.murphy21@imperial.ac.uk>
2021-12-02 15:05:17 +0000

    Working miniproject compile script

--- MiniProject/code/run_MiniProject.sh
+++ MiniProject/code/run_MiniProject.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # Desc: Runs MiniProject and compiles LaTeX report
 
+python3 add_ids.py
+
+Rscript Rexplore.R
+
+Rscript ModelFitting.R
+
+latexmk -pdf -output-directory="../results/" ../writeup/main.tex
